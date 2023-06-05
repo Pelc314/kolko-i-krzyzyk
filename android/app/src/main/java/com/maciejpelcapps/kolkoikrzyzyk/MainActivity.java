6271db77ec9f1c2129444cd3583f89cc70f3aed4
Pelc314 <mpepelc@gmail.com>
2023-06-05 20:22:48 +0200

    added capacitor android support

---
+++ android/app/src/main/java/com/maciejpelcapps/kolkoikrzyzyk/MainActivity.java
@@ -0,0 +1,5 @@
+package com.maciejpelcapps.kolkoikrzyzyk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maciejpelcapps.kolkoikrzyzyk',
+  appName: 'kolko-i-krzyzyk',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
